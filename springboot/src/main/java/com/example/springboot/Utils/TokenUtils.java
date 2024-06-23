f14fef6031cc8704e4d7d117054630a488e50808
OYang <2708586076@qq.com>
2024-06-23 17:57:50 +0800

    创建实体与基本架构

--- springboot/src/main/java/com/example/springboot/Utils/TokenUtils.java
+++ springboot/src/main/java/com/example/springboot/Utils/TokenUtils.java
@@ -31,7 +31,7 @@ public class TokenUtils {
      * @return
      */
     public static String genToken(String adminId, String sign) {
-        return JWT.create().withAudience(adminId) // 将 user id 保存到 token 里面,作为载荷
+        return JWT.create().withAudience(adminId) //将 user id 保存到 token 里面,作为载荷
                 .withExpiresAt(DateUtil.offsetHour(new Date(), 2)) // 2小时后token过期
                 .sign(Algorithm.HMAC256(sign)); // 以 password 作为 token 的密钥
     }
