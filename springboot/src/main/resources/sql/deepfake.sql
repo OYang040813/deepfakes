d68677b41a688e798b6d79b02f9fdb3289e82a59
OYang <2708586076@qq.com>
2024-07-01 16:21:28 +0800

    完善系统

--- springboot/src/main/resources/sql/deepfake.sql
+++ springboot/src/main/resources/sql/deepfake.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50735
  File Encoding         : 65001
 
- Date: 30/06/2024 12:22:03
+ Date: 01/07/2024 16:21:21
 */
 
 SET NAMES utf8mb4;
@@ -30,29 +30,38 @@ CREATE TABLE `audio`  (
   `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
   `size` double(100, 0) UNSIGNED NULL DEFAULT NULL COMMENT '大小',
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '已检测？未检测？正在检测？',
+  `localpath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '本地路径',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of audio
--- ----------------------------
-INSERT INTO `audio` VALUES (7, '有何不可-许嵩.128.mp3', 1, NULL, '2024-06-30 12:13:12', 'http://localhost:9090/api/audio/download/1719720792187?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMDgwNzkyfQ.KNk505ASRnCpKDSikA1l8Ls7P3UOUTWOudE0LCl2uXA', NULL, '-1');
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for dectection
 -- ----------------------------
 DROP TABLE IF EXISTS `dectection`;
 CREATE TABLE `dectection`  (
-  `id` int(11) NOT NULL COMMENT '检测任务id主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '检测任务id主键',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   `createtime` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
   `updatetime` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
   `result` double(255, 0) NULL DEFAULT NULL COMMENT '检测结果',
-  `style` int(11) NULL DEFAULT NULL COMMENT '检测类型',
+  `style` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '检测类型',
   `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '检测所用时间',
   `pid` int(11) NULL DEFAULT NULL COMMENT '父id',
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '检测状态',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
+  `cardnum` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '卡号',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dectection
+-- ----------------------------
+INSERT INTO `dectection` VALUES (8, '1.jpg', '2024-07-01 11:09:35', '2024-07-01 11:09:35', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719803361212?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTYzMzYxfQ.SGVRbo33pvt1BrJ7mNHECG_x6Ay_s53ffZT193koh20', '202404201670051966');
+INSERT INTO `dectection` VALUES (9, '2.jpg', '2024-07-01 11:09:35', '2024-07-01 11:09:35', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719803368845?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTYzMzY4fQ.swGk4MYGqhBZF-MY8BO7oFkqrklmwlECpIQ78J1Vakk', '202404201670051966');
+INSERT INTO `dectection` VALUES (10, '1.jpg', '2024-07-01 12:55:11', '2024-07-01 12:55:11', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719809709181?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5NzA5fQ.ymIx59EgGhGr1ehWM1yEBb09QNMwcP6w2DQqHY12HIo', '202404201670051966');
+INSERT INTO `dectection` VALUES (11, '1.mp4', '2024-07-01 12:58:12', '2024-07-01 12:58:12', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/video/download/1719809889949?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5ODkwfQ.6EazuuYaWzUc9cdXVLyo-kNCs_t_YJFcAKY0Fm4PX3Q', '202404201670051966');
+INSERT INTO `dectection` VALUES (12, '1.jpg', '2024-07-01 15:50:08', '2024-07-01 15:50:08', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719820205870?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTgwMjA1fQ.JTwoms9BVrJCjNLk8Mlsb7qgIleddXMMp5o3DVPoG7w', '202404201670051966');
+INSERT INTO `dectection` VALUES (13, '有何不可-许嵩.128.mp3', '2024-07-01 15:53:17', '2024-07-01 15:53:17', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/audio/download/1719809942553?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5OTQyfQ.PkW469F6H1reH2BOJ5noHvrOnK_WyetjXLr8LJJ2MtE', '202404201670051966');
 
 -- ----------------------------
 -- Table structure for image
@@ -67,15 +76,9 @@ CREATE TABLE `image`  (
   `size` double(100, 0) UNSIGNED NULL DEFAULT NULL COMMENT '大小',
   `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '已检测？未检测？正在检测？',
+  `localpath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '本地路径',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 97 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of image
--- ----------------------------
-INSERT INTO `image` VALUES (91, '1.jpg', 2, NULL, '2024-06-29 23:32:55', NULL, 'http://localhost:9090/api/image/download/1719675174419?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyIiwiZXhwIjoxNzIwMDM1MTc0fQ.2OP6bV6qSflML6mDi0iM7k6b05G1dspHRwYdGf-UNog', '-1');
-INSERT INTO `image` VALUES (92, '1.jpg', 2, NULL, '2024-06-29 23:33:06', NULL, 'http://localhost:9090/api/image/download/1719675186371?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyIiwiZXhwIjoxNzIwMDM1MTg2fQ.l6CKcMS1DLYtNpJ1OIcc2D5EJucCdHTDo2bUvVT1nug', '-1');
-INSERT INTO `image` VALUES (96, '1.jpg', 1, NULL, '2024-06-30 11:48:20', NULL, 'http://localhost:9090/api/image/download/1719719299563?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMDc5Mjk5fQ.hCHSi8mEOeCnd4HchCg3fpx6rXwKo3hoYTnAFxXyDf8', '-1');
+) ENGINE = InnoDB AUTO_INCREMENT = 107 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for message
@@ -125,7 +128,7 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '123456789@163.com', '123456', '13421594388', '男', 66, '', '2024-04-20 11:17:40', '2024-06-30 11:31:26', '202404201670051966', 00000000001, 'http://localhost:9090/api/user/download/1719718285566?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMDc4Mjg1fQ.EzFp7sLj-732TgSH1b-fhwPL2oxTh_WnnpAsO8I3k00', '2004-10-01');
+INSERT INTO `user` VALUES (1, 'admin', '123456789@163.com', '123456', '13421594388', '男', 66, '', '2024-04-20 11:17:40', '2024-07-01 16:08:11', '202404201670051966', 00000000001, 'http://localhost:9090/api/user/download/1719821291200?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTgxMjkxfQ.ht77yAPYE6xsowmXSRzTb7hW8PkFjWVqjqHEVx8YVIQ', '2004-10-01');
 INSERT INTO `user` VALUES (2, '用户1', 'zhang@163.com', '123456', '13421594386', '男', 22, '西安交通大学', '2024-04-20 11:37:07', '2024-06-29 17:52:48', '20240420443544878', 00000000000, NULL, NULL);
 INSERT INTO `user` VALUES (3, '用户2', 'kexu@sb.com', 'ae54a5abf73761b9bd320607721f3509', '13421212121', '男', 18, '', '2024-05-07 11:00:49', '2024-05-08 00:44:54', '202405072054057603', 00000000000, NULL, NULL);
 INSERT INTO `user` VALUES (4, '用户3', 'hongzihao@163.com', '123123', '13421334433', '男', 14, '华南理工大学', '2024-05-08 00:45:34', '2024-05-08 16:49:51', '20240508800959075', 00000000000, NULL, NULL);
@@ -143,12 +146,8 @@ CREATE TABLE `video`  (
   `size` double(100, 0) UNSIGNED NULL DEFAULT NULL COMMENT '大小',
   `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '已检测？未检测？正在检测？',
+  `localpath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '本地路径',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of video
--- ----------------------------
-INSERT INTO `video` VALUES (21, '1.mp4', 1, NULL, '2024-06-30 12:12:34', NULL, 'http://localhost:9090/api/video/download/1719720753986?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMDgwNzU0fQ.zb7onKkUxa8lw6pi4OE4_xBt2MlL_tU8f_jinuVY-NM', '-1');
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
