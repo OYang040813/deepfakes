e0f51010ea01482026bd605d64e2c2c6f799413c
OYang <2708586076@qq.com>
2024-07-01 22:11:38 +0800

    打包

--- springboot/src/main/resources/sql/deepfake.sql
+++ springboot/src/main/resources/sql/deepfake.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50735
  File Encoding         : 65001
 
- Date: 01/07/2024 16:21:21
+ Date: 01/07/2024 22:11:30
 */
 
 SET NAMES utf8mb4;
@@ -50,18 +50,19 @@ CREATE TABLE `dectection`  (
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '检测状态',
   `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
   `cardnum` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '卡号',
+  `localpath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '本地路径',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of dectection
 -- ----------------------------
-INSERT INTO `dectection` VALUES (8, '1.jpg', '2024-07-01 11:09:35', '2024-07-01 11:09:35', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719803361212?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTYzMzYxfQ.SGVRbo33pvt1BrJ7mNHECG_x6Ay_s53ffZT193koh20', '202404201670051966');
-INSERT INTO `dectection` VALUES (9, '2.jpg', '2024-07-01 11:09:35', '2024-07-01 11:09:35', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719803368845?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTYzMzY4fQ.swGk4MYGqhBZF-MY8BO7oFkqrklmwlECpIQ78J1Vakk', '202404201670051966');
-INSERT INTO `dectection` VALUES (10, '1.jpg', '2024-07-01 12:55:11', '2024-07-01 12:55:11', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719809709181?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5NzA5fQ.ymIx59EgGhGr1ehWM1yEBb09QNMwcP6w2DQqHY12HIo', '202404201670051966');
-INSERT INTO `dectection` VALUES (11, '1.mp4', '2024-07-01 12:58:12', '2024-07-01 12:58:12', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/video/download/1719809889949?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5ODkwfQ.6EazuuYaWzUc9cdXVLyo-kNCs_t_YJFcAKY0Fm4PX3Q', '202404201670051966');
-INSERT INTO `dectection` VALUES (12, '1.jpg', '2024-07-01 15:50:08', '2024-07-01 15:50:08', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719820205870?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTgwMjA1fQ.JTwoms9BVrJCjNLk8Mlsb7qgIleddXMMp5o3DVPoG7w', '202404201670051966');
-INSERT INTO `dectection` VALUES (13, '有何不可-许嵩.128.mp3', '2024-07-01 15:53:17', '2024-07-01 15:53:17', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/audio/download/1719809942553?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5OTQyfQ.PkW469F6H1reH2BOJ5noHvrOnK_WyetjXLr8LJJ2MtE', '202404201670051966');
+INSERT INTO `dectection` VALUES (8, '1.jpg', '2024-07-01 11:09:35', '2024-07-01 11:09:35', 100, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719803361212?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTYzMzYxfQ.SGVRbo33pvt1BrJ7mNHECG_x6Ay_s53ffZT193koh20', '202404201670051966', NULL);
+INSERT INTO `dectection` VALUES (9, '2.jpg', '2024-07-01 11:09:35', '2024-07-01 11:09:35', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719803368845?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTYzMzY4fQ.swGk4MYGqhBZF-MY8BO7oFkqrklmwlECpIQ78J1Vakk', '202404201670051966', NULL);
+INSERT INTO `dectection` VALUES (10, '1.jpg', '2024-07-01 12:55:11', '2024-07-01 12:55:11', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719809709181?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5NzA5fQ.ymIx59EgGhGr1ehWM1yEBb09QNMwcP6w2DQqHY12HIo', '202404201670051966', NULL);
+INSERT INTO `dectection` VALUES (11, '1.mp4', '2024-07-01 12:58:12', '2024-07-01 12:58:12', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/video/download/1719809889949?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5ODkwfQ.6EazuuYaWzUc9cdXVLyo-kNCs_t_YJFcAKY0Fm4PX3Q', '202404201670051966', NULL);
+INSERT INTO `dectection` VALUES (12, '1.jpg', '2024-07-01 15:50:08', '2024-07-01 15:50:08', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/image/download/1719820205870?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTgwMjA1fQ.JTwoms9BVrJCjNLk8Mlsb7qgIleddXMMp5o3DVPoG7w', '202404201670051966', NULL);
+INSERT INTO `dectection` VALUES (13, '有何不可-许嵩.128.mp3', '2024-07-01 15:53:17', '2024-07-01 15:53:17', NULL, '图像检测', NULL, 1, '等待检测', 'http://localhost:9090/api/audio/download/1719809942553?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTY5OTQyfQ.PkW469F6H1reH2BOJ5noHvrOnK_WyetjXLr8LJJ2MtE', '202404201670051966', NULL);
 
 -- ----------------------------
 -- Table structure for image
@@ -128,7 +129,7 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '123456789@163.com', '123456', '13421594388', '男', 66, '', '2024-04-20 11:17:40', '2024-07-01 16:08:11', '202404201670051966', 00000000001, 'http://localhost:9090/api/user/download/1719821291200?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTgxMjkxfQ.ht77yAPYE6xsowmXSRzTb7hW8PkFjWVqjqHEVx8YVIQ', '2004-10-01');
+INSERT INTO `user` VALUES (1, 'admin', '123456789@163.com', '123456', '13421594388', '男', 66, '', '2024-04-20 11:17:40', '2024-07-01 16:53:01', '202404201670051966', 00000000001, 'http://localhost:9090/api/user/download/1719821291200?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNzIwMTgxMjkxfQ.ht77yAPYE6xsowmXSRzTb7hW8PkFjWVqjqHEVx8YVIQ', '2004-10-01');
 INSERT INTO `user` VALUES (2, '用户1', 'zhang@163.com', '123456', '13421594386', '男', 22, '西安交通大学', '2024-04-20 11:37:07', '2024-06-29 17:52:48', '20240420443544878', 00000000000, NULL, NULL);
 INSERT INTO `user` VALUES (3, '用户2', 'kexu@sb.com', 'ae54a5abf73761b9bd320607721f3509', '13421212121', '男', 18, '', '2024-05-07 11:00:49', '2024-05-08 00:44:54', '202405072054057603', 00000000000, NULL, NULL);
 INSERT INTO `user` VALUES (4, '用户3', 'hongzihao@163.com', '123123', '13421334433', '男', 14, '华南理工大学', '2024-05-08 00:45:34', '2024-05-08 16:49:51', '20240508800959075', 00000000000, NULL, NULL);
